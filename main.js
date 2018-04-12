18459842dd2e639b3cd6d55180057c7e24f640a8
Anton Belodedenko <anton.belodedenko@gmail.com>
2018-04-12 14:48:10 -0700

    stop Jeckyll collisions

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ function createWindow () {
 
   mainWindow = new BrowserWindow(windowOptions)
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '_index.html'),
     protocol: 'file:',
     slashes: true
   }))
